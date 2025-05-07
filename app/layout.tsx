b8ab1c2cd5e056fcaa3184473e6b840793dcd32c
tachyonok <tachyonparticle123@gmail.com>
2025-05-07 11:39:55 +0530

    anal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -34,6 +35,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+    <Analytics />
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} font-sans antialiased`}
       >
